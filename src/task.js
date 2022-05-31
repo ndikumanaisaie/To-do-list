3e383b4451115a11432b706dcc6b94cbc3481873
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-06-01 00:42:36 +0100

    fix linter errors

--- src/task.js
+++ src/task.js
@@ -1,34 +1,33 @@
 // import data from './data.js';
 
 export default () => {
-  let data = [];
+  const data = [];
 
   data.push({
-      id: 0,
-      completed: false,
-      description: 'list1'
-    },
-    {
-      id: 1,
-      completed: false,
-      description: 'list2'
-    },
-    {
-      id: 2,
-      completed: false,
-      description: 'list3'
-    }
-  );
+    id: 0,
+    completed: false,
+    description: 'list1',
+  },
+  {
+    id: 1,
+    completed: false,
+    description: 'list2',
+  },
+  {
+    id: 2,
+    completed: false,
+    description: 'list3',
+  });
   const div = document.createElement('div');
   const container = document.querySelector('.tasks');
   div.classList.add('task-items');
-  
+
   let content = '';
   data.forEach((d) => {
-    content  += `<div class="task-item list"><div><input type="checkbox" id="task-item" name="task" value="task">
+    content += `<div class="task-item list"><div><input type="checkbox" id="task-item" name="task" value="task">
     <label for="task-item"> ${d.description}</label></div><i class="fa-solid fa-ellipsis-vertical"></i></div>
     <div class="grey-rule"></div>`;
-  })
+  });
   div.innerHTML = content;
   container.appendChild(div);
-}
+};
