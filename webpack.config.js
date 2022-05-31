38950e165a909e40d5dd04342aeefa61aa52a4fe
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-05-31 15:09:57 +0100

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output:{
+    filename: '[name].js',
+    path: __dirname + '/build',
+    chunkFilename: '[id].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
