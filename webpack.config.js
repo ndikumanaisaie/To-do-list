b01f43e30e65963236172d7291d9eea124ae7b88
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-06-06 19:49:02 +0100

    Change dist folder to docs

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: '[name].js',
-    path: `${__dirname}/dist`,
+    path: `${__dirname}/docs`,
     chunkFilename: '[id].[chunkhash].js',
   },
   module: {
@@ -18,7 +18,7 @@ module.exports = {
     ],
   },
   devServer: {
-    static: './dist',
+    static: './docs',
   },
   plugins: [
     new HtmlWebpackPlugin({
