dfc0bfc457bf1c075f2ec0076a4cb12ee2b65725
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-06-09 23:17:45 +0100

    Add edit test function

---
+++ src/__test__/edit.test.js
@@ -0,0 +1,18 @@
+import LocalStorage from '../__mocks__/storage.js';
+
+const newElement = 'kamba';
+const store = new LocalStorage();
+const dataArray = store.getItems();
+function edit(i) {
+  const listObject = dataArray.find((x) => x.index === Number(i));
+  const index = dataArray.indexOf(listObject);
+  dataArray[index].description = newElement;
+  store.setItems(dataArray);
+}
+
+describe('Edit data from storage', () => {
+  edit(1);
+  test('Check if the element is edited', () => {
+    expect(dataArray[0].description).toBe('kamba');
+  });
+});
