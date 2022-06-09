9bd8cf289b835a788f32fd909952f42ffcf32f22
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-06-09 23:34:53 +0100

    Add update and clearCompleted tests

--- src/__test__/update.test.js
+++ src/__test__/update.test.js
@@ -1,19 +1,23 @@
 import LocalStorage from '../__mocks__/storage.js';
 
-const newElement = 'kamba';
 const store = new LocalStorage();
 const dataArray = store.getItems();
 
-function edit(i) {
+function updateCompleted(i) {
   const listObject = dataArray.find((x) => x.index === Number(i));
   const index = dataArray.indexOf(listObject);
-  dataArray[index].description = newElement;
-  store.setItems(dataArray);
+  if (!dataArray[index].completed) {
+    dataArray[index].completed = true;
+    store.setItems(dataArray);
+  } else {
+    dataArray[index].completed = false;
+    store.setItems(dataArray);
+  }
 }
 
 describe('Edit data from storage', () => {
-  edit(1);
+  updateCompleted(1);
   test('Check if the element is edited', () => {
-    expect(dataArray[0].description).toBe('kamba');
+    expect(dataArray[0].completed).toBe(true);
   });
 });
